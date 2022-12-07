18a28a399a5fa6e80666135332427b8bd8f5c7ea
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2022-12-06 22:53:37 -0300

    se dejan funciones, objetos y simulación referentes a pre-entrega2

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 /*********************************************************************************************************************************************************/
-/****************************************************************** DECLARACIÓN DE CONSTANTES ************************************************************/
-/*********************************************************************************************************************************************************/
-
-const USUARIO_1 = "juan"
-const CONTRASEÑA_1 = "123"
-
-const USUARIO_2 = "pedro"
-const CONTRASEÑA_2 = "456"
-
-const USUARIO_3 = "pablo"
-const CONTRASEÑA_3 = "789"
-
-const STOCK_MC5_12 = 1000; 
-const STOCK_MC7_12 = 1000; 
-const STOCK_MC12_12 = 1000; 
-
-/*********************************************************************************************************************************************************/
 /**************************************************************** DECLARACIÓN DE CONSTRUCTORES ***********************************************************/
 /*********************************************************************************************************************************************************/
 
 //constructor de objetos producto
-function crearProducto (marca, modelo, precio, disponible){
-    this.marca = marca.toUpperCase();
-    this.modelo = modelo.toUpperCase();
-    this.precio = parseFloat (precio)
-    this.disponible = disponible;
+class crearProducto {
+    constructor(marca, modelo, precio, disponible) {
+        this.marca = marca.toUpperCase()
+        this.modelo = modelo.toUpperCase()
+        this.precio = parseFloat(precio)
+        this.disponible = disponible
+    }
 }
 
 /*********************************************************************************************************************************************************/
@@ -37,89 +22,18 @@ const carrito = [];
 /*********************************************************************************************************************************************************/
 /****************************************************************** DECLARACIÓN DE FUNCIONES *************************************************************/
 /*********************************************************************************************************************************************************/
-
-/* Función básica para comprobar nombre de usuario y contraseña */
-function comprobarUsuario (){
-    let usuario = prompt("Ingrese nombre de usuario")
-    let contraseña = prompt("Ingrese Contraseña")   
-    
-    switch (usuario){
-        case USUARIO_1:
-            if (contraseña === CONTRASEÑA_1){
-                
-                alert ("logueado correctamente")
-                break;
-            }else {
-                alert ("contraseña incorrecta")
-                break;
-            }
-        case USUARIO_2:
-            if (contraseña === CONTRASEÑA_2){
-                
-                alert ("logueado correctamente")
-                break;
-            }else {
-                console.log ("contraseña incorrecta")
-                break;
-            }
-        case USUARIO_3:
-            if (contraseña === CONTRASEÑA_3){
-                
-                alert ("logueado correctamente")
-                break;
-            }else {
-                alert ("contraseña incorrecta")
-                break;
-            }
-        default:
-            alert ("usuario y/o contraseña incorrectos")
-            break;
-    }
-}
-
-/* Función para sumar productos dentro del carrito de compras */
-function sumarProducto() {
-    let subtotal = 0
-    let total = 0
-    let cantidad = 0
-    let precio = 0
-    let continuar = 0
-    
-
-    do {
-        cantidad = parseInt(prompt("Ingrese cantidad de productos"))
-        precio = parseFloat(prompt("Ingrese monto de producto"))
-        subtotal = cantidad * precio
-        total = total + subtotal
-        continuar = prompt (`Subtotal: ${subtotal}$ - ¿Desea seguir agregando productos? si/no`)  
-        while (true){
-            if(continuar === "si"){
-                break;
-            } else if (continuar === "no") {
-                break;
-                } else{
-                    alert("Opción no válida, vuelva a intentarlo")
-                    continuar = prompt (`Subtotal: ${subtotal}$ - ¿Desea seguir agregando productos? si/no`)
-                }
-        }
-    } while (continuar === "si");
-    alert(`Total: ${total}$`)
-}
-
 function agregarProductoCarrito () {
     let marca = ""
     let modelo = ""
     let precio = 0
     let disponible = true
     let continuar = ""
-    let i = 0
 
     do {
         marca = prompt("Ingrese marca de batería")
         modelo = prompt ("Ingrese modelo de batería")
         precio = parseFloat(prompt("Ingrese monto de producto"))
-        let aux = new crearProducto (marca, modelo, precio, disponible);
-        carrito.push (aux)
+        carrito.push (new crearProducto (marca, modelo, precio, disponible))
         
         continuar = prompt ("¿Desea seguir agregando productos? si/no")  
         continuar = continuar.toUpperCase()
@@ -131,17 +45,33 @@ function agregarProductoCarrito () {
                 } else{
                     alert("Opción no válida, vuelva a intentarlo")
                     continuar = prompt ("¿Desea seguir agregando productos? si/no") 
+                    continuar = continuar.toUpperCase()
                 }
         }
     } while (continuar === "SI");
-    console.log(carrito)
+    console.log("Carrito de compras:")
+    carrito.forEach((elemento)=>{
+        console.log(elemento)
+    })
 }
 
 /*********************************************************************************************************************************************************/
 /********************************************************************* PRUEBA DE FUNCIONES ***************************************************************/
 /*********************************************************************************************************************************************************/
-//comprobarUsuario ()
-//sumarProducto ()
 agregarProductoCarrito () 
 const precios = carrito.map (producto => producto.precio)
-console.log (precios)
+
+
+const ofertas = carrito.filter(producto => producto.precio < 50)
+console.log("productos en oferta:")
+console.log(ofertas)
+
+/*
+//Otra alternativa para sumar los precios dentro del array
+let total = 0
+for (let index=0; index < precios.length; index++){
+    total += precios[index]
+}
+*/
+const total = precios.reduce((acumulador, elemento)=> acumulador + elemento, 0)
+alert(`Total: ${total}$ - Gracias por su compra!`)
