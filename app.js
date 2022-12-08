2d7f0dd615450b6fa5f8a766e06c7e796b9d6925
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2022-12-07 21:02:59 -0300

    agregando comentarios al archivo para realizar segunda pre-entrega

--- app.js
+++ app.js
@@ -22,22 +22,26 @@ const carrito = [];
 /*********************************************************************************************************************************************************/
 /****************************************************************** DECLARACIÓN DE FUNCIONES *************************************************************/
 /*********************************************************************************************************************************************************/
+//función para agregar productos al carrito
 function agregarProductoCarrito () {
+    //Se declaran variables locales
     let marca = ""
     let modelo = ""
     let precio = 0
     let disponible = true
     let continuar = ""
 
-    do {
+    do {//bucle para iniciar el simulador
+        //solicito opciones al usuario
         marca = prompt("Ingrese marca de batería")
         modelo = prompt ("Ingrese modelo de batería")
         precio = parseFloat(prompt("Ingrese monto de producto"))
-        carrito.push (new crearProducto (marca, modelo, precio, disponible))
+        carrito.push (new crearProducto (marca, modelo, precio, disponible)) //se agrega un objeto al array
         
         continuar = prompt ("¿Desea seguir agregando productos? si/no")  
-        continuar = continuar.toUpperCase()
-        while (true){           
+        continuar = continuar.toUpperCase() //paso a mayúscula la opción ingresada
+        while (true){ 
+            //se evalúa opción hasta que ingrese "SI" o "NO"         
             if(continuar === "SI"){
                 break;
             } else if (continuar === "NO") {
@@ -49,6 +53,7 @@ function agregarProductoCarrito () {
                 }
         }
     } while (continuar === "SI");
+    //Imprimo el carrito con los produtos agregados
     console.log("Carrito de compras:")
     carrito.forEach((elemento)=>{
         console.log(elemento)
@@ -58,11 +63,11 @@ function agregarProductoCarrito () {
 /*********************************************************************************************************************************************************/
 /********************************************************************* PRUEBA DE FUNCIONES ***************************************************************/
 /*********************************************************************************************************************************************************/
-agregarProductoCarrito () 
-const precios = carrito.map (producto => producto.precio)
+agregarProductoCarrito () // pruebo función creada
+const precios = carrito.map (producto => producto.precio) // genero un map solo con los precios para sumarlos mas adelante
 
 
-const ofertas = carrito.filter(producto => producto.precio < 50)
+const ofertas = carrito.filter(producto => producto.precio < 50) //filtro los productos que tengan precio menor a 50$ y lo muestro por consola
 console.log("productos en oferta:")
 console.log(ofertas)
 
@@ -73,5 +78,5 @@ for (let index=0; index < precios.length; index++){
     total += precios[index]
 }
 */
-const total = precios.reduce((acumulador, elemento)=> acumulador + elemento, 0)
+const total = precios.reduce((acumulador, elemento)=> acumulador + elemento, 0) //sumo los precios del map y los muestro al usuario
 alert(`Total: ${total}$ - Gracias por su compra!`)
