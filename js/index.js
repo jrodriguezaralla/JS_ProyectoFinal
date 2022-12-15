94cfae0c38d2bc0c0a367106391c8b6a9f39a6d0
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2022-12-15 15:59:40 -0300

    se crea la función mostrar productos () para cargar los elementos de forma dinamica.

--- js/index.js
+++ js/index.js
@@ -1,87 +1,54 @@
 /*********************************************************************************************************************************************************/
-/**************************************************************** DECLARACIÓN DE CONSTRUCTORES ***********************************************************/
+/****************************************************************** DECLARACIÓN DE VARIABLES *************************************************************/
 /*********************************************************************************************************************************************************/
-
-//constructor de objetos producto
-// class CrearProducto {
-//     constructor(marca, modelo, precio, disponible) {
-//         this.marca = marca.toUpperCase()
-//         this.modelo = modelo.toUpperCase()
-//         this.precio = parseFloat(precio)
-//         this.disponible = disponible
-//     }
-// }
+// Contenedor de listado de productos
+let listado = document.querySelector("#listado") 
 
 /*********************************************************************************************************************************************************/
 /******************************************************************** DECLARACIÓN DE ARRAYS **************************************************************/
 /*********************************************************************************************************************************************************/
 
-//const carrito = [];
+const carrito = []
 
 
 /*********************************************************************************************************************************************************/
 /****************************************************************** DECLARACIÓN DE FUNCIONES *************************************************************/
 /*********************************************************************************************************************************************************/
-//función para agregar productos al carrito
-// function agregarProductoCarrito () {
-//     //Se declaran variables locales
-//     let marca = ""
-//     let modelo = ""
-//     let precio = 0
-//     let disponible = true
-//     let continuar = ""
+function mostrarProductos () {
+    productos.forEach(producto =>{
+        let div = document.createElement("div")
+        div.className = "card"
+        div.innerHTML = `<div class="card-header">
+                            <h5 class="m-0 fw-semibold fs-5">${producto.modelo}</h5>
+                        </div>
+                        <div class="d-flex flex-column justify-content-between align-items-center p-2">    
+                            <img class="card-img" src="./imagenes/${producto.imagen}" alt="img_${producto.modelo}">
+                            <p class="fw-lighter">${producto.descripcion}</p>
+                            <p class="fw-bold fst-italic fs-5">${producto.precio} USD</p>
+                            <div class="d-flex justify-content-center">
+                                <button id="${producto.id}" class="btnAgregarCarrito btn btn-dark" type="submit">Agregar al carrito</button>
+                            </div>
+                        </div>`
+        listado.append(div)
+    })
 
-//     do {//bucle para iniciar el simulador
-//         //solicito opciones al usuario
-//         marca = prompt("Ingrese marca de batería")
-//         modelo = prompt ("Ingrese modelo de batería")
-//         precio = parseFloat(prompt("Ingrese monto de producto"))
-//         carrito.push (new CrearProducto (marca, modelo, precio, disponible)) //se agrega un objeto al array
-        
-//         continuar = prompt ("¿Desea seguir agregando productos? si/no")  
-//         continuar = continuar.toUpperCase() //paso a mayúscula la opción ingresada
-//         while (true){ 
-//             //se evalúa opción hasta que ingrese "SI" o "NO"         
-//             if(continuar === "SI"){
-//                 break;
-//             } else if (continuar === "NO") {
-//                 break;
-//                 } else{
-//                     alert("Opción no válida, vuelva a intentarlo")
-//                     continuar = prompt ("¿Desea seguir agregando productos? si/no") 
-//                     continuar = continuar.toUpperCase()
-//                 }
-//         }
-//     } while (continuar === "SI");
-//     //Imprimo el carrito con los produtos agregados
-//     console.log("Carrito de compras:")
-//     carrito.forEach((elemento)=>{
-//         console.log(elemento)
-//     })
-// }
+    let btnAgregarCarrito = document.querySelectorAll('.btnAgregarCarrito') // NodeList = [button#1, button#2 .... , button#n]
+    btnAgregarCarrito.forEach(el =>{
+        el.addEventListener('click',(e) => {
+            agregarProductoCarrito (e.target.id)
+        })
+    })
+}
+
+function agregarProductoCarrito (id) {
+    const productoSelecionado = productos.find(elemento => elemento.id === parseInt(id)) // busco por el id el producto que el usuario eligio agregar
+    carrito.push(productoSelecionado) // lo pusheo al carrito
+}
+
+
+
 
 /*********************************************************************************************************************************************************/
 /********************************************************************* PRUEBA DE FUNCIONES ***************************************************************/
 /*********************************************************************************************************************************************************/
-//agregarProductoCarrito () // pruebo función creada
-//const precios = carrito.map (producto => producto.precio) // genero un map solo con los precios para sumarlos mas adelante
-
-
-//const ofertas = carrito.filter(producto => producto.precio < 50) //filtro los productos que tengan precio menor a 50$ y lo muestro por consola
-//console.log("productos en oferta:")
-//console.log(ofertas)
-
-/*
-//Otra alternativa para sumar los precios dentro del array
-let total = 0
-for (let index=0; index < precios.length; index++){
-    total += precios[index]
-}
-*/
-//const total = precios.reduce((acumulador, elemento)=> acumulador + elemento, 0) //sumo los precios del map y los muestro al usuario
-//alert(`Total: ${total}$ - Gracias por su compra!`)
-
-let listado = document.querySelector("#listado")
-console.log(bateriasYuasa)
-console.log(bateriasMicrocell)
-console.log(bateriasSBS)
+mostrarProductos();
