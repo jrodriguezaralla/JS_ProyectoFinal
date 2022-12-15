b37b1ca569b729faadc08a927399c3c3a8b4cd0e
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2022-12-15 16:00:52 -0300

    se eliminaron las card que sera agregadas de forma dinámica desde JS

--- js/datos.js
+++ js/datos.js
@@ -1,5 +1,7 @@
-bateriasYuasa = [
+productos = [
+    /************************************************************************ Baterias Yuasa *****************************************************************************************/
     {
+        "id" : 1,
         "marca" : "YUASA",
         "modelo" : "NPX25",
         "imagen" : "NPX25.jpg",
@@ -7,13 +9,15 @@ bateriasYuasa = [
         "precio" : 30.50
     },
     {
+        "id" : 2,
         "marca" : "YUASA",
         "modelo" : "NP7.2-12",
-        "imagen" : "N7.2-12.jpg",
+        "imagen" : "NP7.2-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-7ah",
         "precio" : 38
     },
     {
+        "id" : 3,
         "marca" : "YUASA",
         "modelo" : "NPW45-12",
         "imagen" : "NPW45-12.jpg",
@@ -21,6 +25,7 @@ bateriasYuasa = [
         "precio" : 47
     },
     {
+        "id" : 4,
         "marca" : "YUASA",
         "modelo" : "NP12-12",
         "imagen" : "NP12-12.jpg",
@@ -28,6 +33,7 @@ bateriasYuasa = [
         "precio" : 73
     },
     {
+        "id" : 5,
         "marca" : "YUASA",
         "modelo" : "NP18-12",
         "imagen" : "NP18-12.jpg",
@@ -35,6 +41,7 @@ bateriasYuasa = [
         "precio" : 81
     },
     {
+        "id" : 6,
         "marca" : "YUASA",
         "modelo" : "NP24-12",
         "imagen" : "NP24-12.jpg",
@@ -42,6 +49,7 @@ bateriasYuasa = [
         "precio" : 140
     },
     {
+        "id" : 7,
         "marca" : "YUASA",
         "modelo" : "NP38-12",
         "imagen" : "NP38-12.jpg",
@@ -49,6 +57,7 @@ bateriasYuasa = [
         "precio" : 244
     },
     {
+        "id" : 8,
         "marca" : "YUASA",
         "modelo" : "NP65-12",
         "imagen" : "NP65-12.jpg",
@@ -56,6 +65,7 @@ bateriasYuasa = [
         "precio" : 381
     },
     {
+        "id" : 9,
         "marca" : "YUASA",
         "modelo" : "UXH90-12",
         "imagen" : "UXH90-12.jpg",
@@ -63,16 +73,16 @@ bateriasYuasa = [
         "precio" : 473
     },
     {
+        "id" : 10,
         "marca" : "YUASA",
         "modelo" : "UXH100-12",
         "imagen" : "UXH100-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-100ah",
         "precio" : 502
-    }
-]
-
-bateriasMicrocell = [
+    },
+/************************************************************************ Baterias Microcell *****************************************************************************************/
     {
+        "id" : 11,
         "marca" : "MICROCELL",
         "modelo" : "MC5-12",
         "imagen" : "MC5-12.jpg",
@@ -80,6 +90,7 @@ bateriasMicrocell = [
         "precio" : 18.50
     },
     {
+        "id" : 12,
         "marca" : "MICROCELL",
         "modelo" : "MC7-12",
         "imagen" : "MC7-12.jpg",
@@ -87,6 +98,7 @@ bateriasMicrocell = [
         "precio" : 24.50
     },
     {
+        "id" : 13,
         "marca" : "MICROCELL",
         "modelo" : "MCX36",
         "imagen" : "MCX36.jpg",
@@ -94,6 +106,7 @@ bateriasMicrocell = [
         "precio" : 30.50
     },
     {
+        "id" : 14,
         "marca" : "MICROCELL",
         "modelo" : "MC12-12",
         "imagen" : "MC12-12.jpg",
@@ -101,6 +114,7 @@ bateriasMicrocell = [
         "precio" : 43
     },
     {
+        "id" : 15,
         "marca" : "MICROCELL",
         "modelo" : "MC17-12",
         "imagen" : "MC17-12.jpg",
@@ -108,6 +122,7 @@ bateriasMicrocell = [
         "precio" : 55
     },
     {
+        "id" : 16,
         "marca" : "MICROCELL",
         "modelo" : "MC24-12",
         "imagen" : "MC24-12.jpg",
@@ -115,6 +130,7 @@ bateriasMicrocell = [
         "precio" : 84
     },
     {
+        "id" : 17,
         "marca" : "MICROCELL",
         "modelo" : "MCE33-12",
         "imagen" : "MCE33-12.jpg",
@@ -122,6 +138,7 @@ bateriasMicrocell = [
         "precio" : 111
     },
     {
+        "id" : 18,
         "marca" : "MICROCELL",
         "modelo" : "MCE40-12",
         "imagen" : "MCE40-12.jpg",
@@ -129,6 +146,7 @@ bateriasMicrocell = [
         "precio" : 127
     },
     {
+        "id" : 19,
         "marca" : "MICROCELL",
         "modelo" : "MCE55-12",
         "imagen" : "MCE55-12.jpg",
@@ -136,6 +154,7 @@ bateriasMicrocell = [
         "precio" : 198
     },
     {
+        "id" : 20,
         "marca" : "MICROCELL",
         "modelo" : "MCE65-12",
         "imagen" : "MCE65-12.jpg",
@@ -143,6 +162,7 @@ bateriasMicrocell = [
         "precio" : 219
     },
     {
+        "id" : 21,
         "marca" : "MICROCELL",
         "modelo" : "MCE75-12",
         "imagen" : "MCE75-12.jpg",
@@ -150,6 +170,7 @@ bateriasMicrocell = [
         "precio" : 257
     },
     {
+        "id" : 22,
         "marca" : "MICROCELL",
         "modelo" : "MCE90-12",
         "imagen" : "MCE90-12.jpg",
@@ -157,6 +178,7 @@ bateriasMicrocell = [
         "precio" : 308
     },
     {
+        "id" : 23,
         "marca" : "MICROCELL",
         "modelo" : "MCE100-12",
         "imagen" : "MCE100-12.jpg",
@@ -164,6 +186,7 @@ bateriasMicrocell = [
         "precio" : 313
     },
     {
+        "id" : 24,
         "marca" : "MICROCELL",
         "modelo" : "MCE120-12",
         "imagen" : "MCE120-12.jpg",
@@ -171,6 +194,7 @@ bateriasMicrocell = [
         "precio" : 338
     },
     {
+        "id" : 25,
         "marca" : "MICROCELL",
         "modelo" : "MCE150-12",
         "imagen" : "MCE150-12.jpg",
@@ -178,16 +202,17 @@ bateriasMicrocell = [
         "precio" : 500
     },
     {
+        "id" : 26,
         "marca" : "MICROCELL",
         "modelo" : "MCE200-12",
         "imagen" : "MCE200-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-200ah",
         "precio" : 628
-    }
-]
-
-bateriasSBS = [
+    },
+    /************************************************************************ Baterias SBS *****************************************************************************************/
+    
     {
+        "id" : 27,
         "marca" : "ENERSYS",
         "modelo" : "SBS C11",
         "imagen" : "SBSC11.jpg",
@@ -195,6 +220,7 @@ bateriasSBS = [
         "precio" : 447
     },
     {
+        "id" : 28,
         "marca" : "ENERSYS",
         "modelo" : "SBS 100F",
         "imagen" : "SBS100F.jpg",
@@ -202,6 +228,7 @@ bateriasSBS = [
         "precio" : 443
     },
     {
+        "id" : 29,
         "marca" : "ENERSYS",
         "modelo" : "SBS 170F",
         "imagen" : "SBS170F.jpg",
@@ -209,6 +236,7 @@ bateriasSBS = [
         "precio" : 563
     },
     {
+        "id" : 30,
         "marca" : "ENERSYS",
         "modelo" : "SBS 190F",
         "imagen" : "SBS190F.jpg",
