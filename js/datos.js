f507fdd374ca43fb55c7ba07f9a4eddadb37bea7
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-01-04 21:57:54 -0300

    arreglando conflictos surgidos del merge con pre-entrega3

--- js/datos.js
+++ js/datos.js
@@ -1,516 +1,276 @@
 productos = [
     /************************************************************************ Baterias Yuasa *****************************************************************************************/
     {
-<<<<<<< HEAD
-        "id" : 1,
-=======
         "id" : 0,
->>>>>>> pre-entrega3
         "marca" : "YUASA",
         "modelo" : "NPX25",
         "imagen" : "NPX25.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-5ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 30.50
-    },
-    {
-        "id" : 2,
-=======
         "cantidad" : 1,
         "precio" : 30.50
     },
     {
         "id" : 1,
->>>>>>> pre-entrega3
         "marca" : "YUASA",
         "modelo" : "NP7.2-12",
         "imagen" : "NP7.2-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-7ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 38
-    },
-    {
-        "id" : 3,
-=======
         "cantidad" : 1,
         "precio" : 38
     },
     {
         "id" : 2,
->>>>>>> pre-entrega3
         "marca" : "YUASA",
         "modelo" : "NPW45-12",
         "imagen" : "NPW45-12.jpg",
         "descripcion" :  "Bateria VRLA PLOMO-CALCIO 12V-8,5ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 47
-    },
-    {
-        "id" : 4,
-=======
         "cantidad" : 1,
         "precio" : 47
     },
     {
         "id" : 3,
->>>>>>> pre-entrega3
         "marca" : "YUASA",
         "modelo" : "NP12-12",
         "imagen" : "NP12-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-12ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 73
-    },
-    {
-        "id" : 5,
-=======
         "cantidad" : 1,
         "precio" : 73
     },
     {
         "id" : 4,
->>>>>>> pre-entrega3
         "marca" : "YUASA",
         "modelo" : "NP18-12",
         "imagen" : "NP18-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-18ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 81
-    },
-    {
-        "id" : 6,
-=======
         "cantidad" : 1,
         "precio" : 81
     },
     {
         "id" : 5,
->>>>>>> pre-entrega3
         "marca" : "YUASA",
         "modelo" : "NP24-12",
         "imagen" : "NP24-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-24ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 140
-    },
-    {
-        "id" : 7,
-=======
         "cantidad" : 1,
         "precio" : 140
     },
     {
         "id" : 6,
->>>>>>> pre-entrega3
         "marca" : "YUASA",
         "modelo" : "NP38-12",
         "imagen" : "NP38-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-38ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 244
-    },
-    {
-        "id" : 8,
-=======
         "cantidad" : 1,
         "precio" : 244
     },
     {
         "id" : 7,
->>>>>>> pre-entrega3
         "marca" : "YUASA",
         "modelo" : "NP65-12",
         "imagen" : "NP65-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-65ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 381
-    },
-    {
-        "id" : 9,
-=======
         "cantidad" : 1,
         "precio" : 381
     },
     {
         "id" : 8,
->>>>>>> pre-entrega3
         "marca" : "YUASA",
         "modelo" : "UXH90-12",
         "imagen" : "UXH90-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-90ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 473
-    },
-    {
-        "id" : 10,
-=======
         "cantidad" : 1,
         "precio" : 473
     },
     {
         "id" : 9,
->>>>>>> pre-entrega3
         "marca" : "YUASA",
         "modelo" : "UXH100-12",
         "imagen" : "UXH100-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-100ah",
-<<<<<<< HEAD
-        "contador" : 0,
-=======
         "cantidad" : 1,
->>>>>>> pre-entrega3
         "precio" : 502
     },
 /************************************************************************ Baterias Microcell *****************************************************************************************/
     {
-<<<<<<< HEAD
-        "id" : 11,
-=======
         "id" : 10,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MC5-12",
         "imagen" : "MC5-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-5ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 18.50
-    },
-    {
-        "id" : 12,
-=======
         "cantidad" : 1,
         "precio" : 18.50
     },
     {
         "id" : 11,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MC7-12",
         "imagen" : "MC7-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-7ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 24.50
-    },
-    {
-        "id" : 13,
-=======
         "cantidad" : 1,
         "precio" : 24.50
     },
     {
         "id" : 12,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCX36",
         "imagen" : "MCX36.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-8ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 30.50
-    },
-    {
-        "id" : 14,
-=======
         "cantidad" : 1,
         "precio" : 30.50
     },
     {
         "id" : 13,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MC12-12",
         "imagen" : "MC12-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-12ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 43
-    },
-    {
-        "id" : 15,
-=======
         "cantidad" : 1,
         "precio" : 43
     },
     {
         "id" : 14,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MC17-12",
         "imagen" : "MC17-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-17ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 55
-    },
-    {
-        "id" : 16,
-=======
         "cantidad" : 1,
         "precio" : 55
     },
     {
         "id" : 15,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MC24-12",
         "imagen" : "MC24-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-24ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 84
-    },
-    {
-        "id" : 17,
-=======
         "cantidad" : 1,
         "precio" : 84
     },
     {
         "id" : 16,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCE33-12",
         "imagen" : "MCE33-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-33ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 111
-    },
-    {
-        "id" : 18,
-=======
         "cantidad" : 1,
         "precio" : 111
     },
     {
         "id" : 17,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCE40-12",
         "imagen" : "MCE40-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-40ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 127
-    },
-    {
-        "id" : 19,
-=======
         "cantidad" : 1,
         "precio" : 127
     },
     {
         "id" : 18,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCE55-12",
         "imagen" : "MCE55-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-55ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 198
-    },
-    {
-        "id" : 20,
-=======
         "cantidad" : 1,
         "precio" : 198
     },
     {
         "id" : 19,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCE65-12",
         "imagen" : "MCE65-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-65ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 219
-    },
-    {
-        "id" : 21,
-=======
         "cantidad" : 1,
         "precio" : 219
     },
     {
         "id" : 20,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCE75-12",
         "imagen" : "MCE75-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-75ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 257
-    },
-    {
-        "id" : 22,
-=======
         "cantidad" : 1,
         "precio" : 257
     },
     {
         "id" : 21,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCE90-12",
         "imagen" : "MCE90-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-90ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 308
-    },
-    {
-        "id" : 23,
-=======
         "cantidad" : 1,
         "precio" : 308
     },
     {
         "id" : 22,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCE100-12",
         "imagen" : "MCE100-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-100ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 313
-    },
-    {
-        "id" : 24,
-=======
         "cantidad" : 1,
         "precio" : 313
     },
     {
         "id" : 23,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCE120-12",
         "imagen" : "MCE120-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-120ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 338
-    },
-    {
-        "id" : 25,
-=======
         "cantidad" : 1,
         "precio" : 338
     },
     {
         "id" : 24,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCE150-12",
         "imagen" : "MCE150-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-150ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 500
-    },
-    {
-        "id" : 26,
-=======
         "cantidad" : 1,
         "precio" : 500
     },
     {
         "id" : 25,
->>>>>>> pre-entrega3
         "marca" : "MICROCELL",
         "modelo" : "MCE200-12",
         "imagen" : "MCE200-12.jpg",
         "descripcion" : "Bateria VRLA PLOMO-CALCIO 12V-200ah",
-<<<<<<< HEAD
-        "contador" : 0,
-=======
         "cantidad" : 1,
->>>>>>> pre-entrega3
         "precio" : 628
     },
     /************************************************************************ Baterias SBS *****************************************************************************************/
     
     {
-<<<<<<< HEAD
-        "id" : 27,
-=======
         "id" : 26,
->>>>>>> pre-entrega3
         "marca" : "ENERSYS",
         "modelo" : "SBS C11",
         "imagen" : "SBSC11.jpg",
         "descripcion" : "Bateria VRLA PLOMO PURO 12V-92ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 447
-    },
-    {
-        "id" : 28,
-=======
         "cantidad" : 1,
         "precio" : 447
     },
     {
         "id" : 27,
->>>>>>> pre-entrega3
         "marca" : "ENERSYS",
         "modelo" : "SBS 100F",
         "imagen" : "SBS100F.jpg",
         "descripcion" : "Bateria VRLA PLOMO PURO 12V-100ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 443
-    },
-    {
-        "id" : 29,
-=======
         "cantidad" : 1,
         "precio" : 443
     },
     {
         "id" : 28,
->>>>>>> pre-entrega3
         "marca" : "ENERSYS",
         "modelo" : "SBS 170F",
         "imagen" : "SBS170F.jpg",
         "descripcion" : "Bateria VRLA PLOMO PURO 12V-170ah",
-<<<<<<< HEAD
-        "contador" : 0,
-        "precio" : 563
-    },
-    {
-        "id" : 30,
-=======
         "cantidad" : 1,
         "precio" : 563
     },
     {
         "id" : 29,
->>>>>>> pre-entrega3
         "marca" : "ENERSYS",
         "modelo" : "SBS 190F",
         "imagen" : "SBS190F.jpg",
         "descripcion" : "Bateria VRLA PLOMO PURO 12V-190ah",
-<<<<<<< HEAD
-        "contador" : 0,
-=======
         "cantidad" : 1,
->>>>>>> pre-entrega3
         "precio" : 632
     }
 ]
